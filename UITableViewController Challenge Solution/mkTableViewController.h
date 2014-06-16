67edb1134a825d3656a9f6efc1f3c1d45d07061a
Mikki Kaushal <mikkikaushal@Mikki-Kaushals-MacBook-Pro.local>
2014-06-16 10:45:14 -0400

    solved the challenge

---
+++ UITableViewController Challenge Solution/mkTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  mkTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Mikki Kaushal on 6/16/14.
+//  Copyright (c) 2014 Mikki Kaushal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mkTableViewController : UITableViewController
+
+@end
